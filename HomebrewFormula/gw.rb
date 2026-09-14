9130dd06a6bbf616def63692b2a09be1e9a93f58
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2026-09-14 13:43:23 +0100

    chore: sync HomebrewFormula com v0.8.2

--- HomebrewFormula/gw.rb
+++ HomebrewFormula/gw.rb
@@ -3,8 +3,8 @@ class Gw < Formula
 
   desc "Google Workspace CLI - Gmail, Calendar, Drive, Sheets, Docs from your terminal"
   homepage "https://github.com/v-gutierrez/gw"
-  url "https://github.com/v-gutierrez/gw/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "b2be8a60493efb39d9bba0bf34b01df958ac7386920813ddddefbedde6301d7d"
+  url "https://github.com/v-gutierrez/gw/archive/refs/tags/v0.8.2.tar.gz"
+  sha256 "cc471cf7363a2881c3f7e54ea678045d0afeeaae7a8f30335fc5cf9b41ca0f79"
   license "MIT"
 
   depends_on "openssl@3"
@@ -266,6 +266,6 @@ class Gw < Formula
   end
 
   test do
-    assert_match "0.8.1", shell_output("#{bin}/gw --version")
+    assert_match "0.8.2", shell_output("#{bin}/gw --version")
   end
 end
